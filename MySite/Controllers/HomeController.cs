84ea8583b21da28da50066b200e2922d3a6fa651
reza moazen <reza.moazen001@gmail.com>
2024-05-13 15:58:34 +0330

    index & contact Added in project

--- MySite/Controllers/HomeController.cs
+++ MySite/Controllers/HomeController.cs
@@ -6,19 +6,12 @@ namespace MySite.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
